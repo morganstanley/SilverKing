624bb70797c0d313b5cb7fa35d6587e8c9bccf1a
Benjamin Holst <Benjamin.Holst@morganstanley.com>
2018-03-06 18:50:19 -0500

    refactor: replaced usages of 'gtest' with variable GTEST_FOLDER_NAME in common.vars

--- build/aws/aws_clean.sh
+++ build/aws/aws_clean.sh
@@ -1,6 +1,13 @@
 #!/bin/ksh
 
+cd ..
+source lib/common.lib
+cd -
+
 rm ~/.ssh/id_rsa*
 rm -rf /tmp
 rm -rf /var/tmp
-rm ~/SilverKing/build/testing/GC_sk_test.env
+rm $TESTING_OUTPUT_DIR/GC_sk_test.env
+rm $BUILD_DIR/zookeeper.out
+# rm -rf $TESTING_OUTPUT_DIR/$GTEST_FOLDER_NAME
+# rm -rf $TESTING_OUTPUT_DIR/$JUNIT_FOLDER_NAME
