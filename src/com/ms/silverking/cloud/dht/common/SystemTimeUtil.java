3647cf78a6511344554a994b08184ef1bf61be6b
Glenn Judd <glenn.judd@morganstanley.com>
2018-04-05 15:18:26 -0400

    Fix system time skew

--- src/com/ms/silverking/cloud/dht/common/SystemTimeUtil.java
+++ src/com/ms/silverking/cloud/dht/common/SystemTimeUtil.java
@@ -4,5 +4,10 @@ import com.ms.silverking.time.SystemTimeSource;
 
 public class SystemTimeUtil {
     public static final SystemTimeSource systemTimeSource 
-                            = SystemTimeSource.createWithMillisOrigin(DHTConstants.nanoOriginTimeInMillis);    
+                            = SystemTimeSource.createWithMillisOrigin(DHTConstants.nanoOriginTimeInMillis);  
+    
+    public static final long systemTimeNanosToEpochMillis(long nanos) {
+		//absTimeNanos = (absTimeMillis - nanoOriginTimeMillis) * nanosPerMilli;
+    	return (nanos / 1000000) + DHTConstants.nanoOriginTimeInMillis;
+    }
 }
