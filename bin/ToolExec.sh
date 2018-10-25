5d3a33a44cb6c7c5ee83dce7a8b8de63911e4222
Benjamin Holst <Benjamin.Holst@morganstanley.com>
2018-10-25 18:16:01 -0400

    made SKAdmin go thru ToolExec. Now all scripts go thru one place. SKAdmin was the last one to fix.

--- bin/ToolExec.sh
+++ bin/ToolExec.sh
@@ -11,9 +11,21 @@ allArgs="$@"
 f_sourceSkConfigAndConfigureClasspath "$__SK_CLASSPATH_TYPE"
 
 toolClass=$__SK_TOOL_EXEC_TOOL_CLASS
-cmd="$skJavaHome/bin/java -ea -cp $classpath $toolClass $allArgs"
-$cmd
-javaExitCode=$?
-
+cmd="$skAdminJavaCommandHeader $skJavaHome/bin/java -ea -cp $classpath $toolClass $allArgs"
+f_logStart "$toolClass" "$allArgs"
+typeset skAdminPattern="SKAdmin"
+if [[ $toolClass =~ ${skAdminPattern}$ ]]; then   # quotes around "SKAdmin$" makes the if statement fail for some reason...
+    export PATH=$PATH:/usr/bin
+    export      skKillCommand=`pwd`/kill_process_and_children.pl
+    export skCheckSKFSCommand=`pwd`/skfs/check_skfs.sh
+    echo $cmd
+    $cmd 2>&1 | tee -a /tmp/${skAdminPattern}.$$.stdout
+    javaExitCode=${PIPESTATUS[0]}
+else
+    $cmd
+    javaExitCode=$?
+fi
+    
+f_logStop "$toolClass" "$allArgs"
 cd $old_dir
 f_exit "$javaExitCode"
