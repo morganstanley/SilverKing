35ac08a2623eeaa5e513ca9d6cd21778bd71b4d6
Glenn Judd <glenn.judd@morganstanley.com>
2019-04-03 16:36:57 -0400

    lwt exit upon uncaught exception

--- src/com/ms/silverking/thread/lwt/LWTUncaughtExceptionHandler.java
+++ src/com/ms/silverking/thread/lwt/LWTUncaughtExceptionHandler.java
@@ -3,11 +3,19 @@ package com.ms.silverking.thread.lwt;
 import java.lang.Thread.UncaughtExceptionHandler;
 
 import com.ms.silverking.log.Log;
+import com.ms.silverking.thread.ThreadUtil;
 
 public class LWTUncaughtExceptionHandler implements UncaughtExceptionHandler {
-
+	private static final int	uncaughtExceptionExitDelayMillis = 4 * 1000;
+	
+	/**
+	 * Display information about the uncaught exception and then force the JVM to exit.
+	 * We prefer fail-stop behavior to unexpected behavior.
+	 */
     @Override
     public void uncaughtException(Thread t, Throwable e) {
         Log.logErrorWarning(e, "Thread "+ t.getName() +" threw an uncaught exception");
+        ThreadUtil.sleep(uncaughtExceptionExitDelayMillis);
+        System.exit(1);
     }
 }
