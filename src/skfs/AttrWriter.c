92895f4eda01681013ba83cef82864a754318a87
Glenn Judd <glenn.judd@morganstanley.com>
2019-04-10 16:03:10 -0400

    minor write time change

--- src/skfs/AttrWriter.c
+++ src/skfs/AttrWriter.c
@@ -218,6 +218,7 @@ SKOperationState::SKOperationState aw_write_attr_direct(AttrWriter *aw, const ch
 	SKAsyncPut	*pPut;
 	int			attempt;
 	unsigned int	seedp;
+    uint64_t    writeTimeMicros;
 	
 	seedp = 0;
 	srfsLog(LOG_FINE, "in aw_write_attr_direct");	
@@ -225,6 +226,7 @@ SKOperationState::SKOperationState aw_write_attr_direct(AttrWriter *aw, const ch
 	pVal = sk_create_val();
 	sk_set_val_zero_copy(pVal, sizeof(FileAttr), (void *)fa);
 	srfsLog(LOG_FINE, "aw_write_attr_direct path %s pVal->m_len %u", path, pVal->m_len);	
+    writeTimeMicros = curTimeMicros();
 	attempt = 0;
 	do {
 		try {
@@ -272,7 +274,7 @@ SKOperationState::SKOperationState aw_write_attr_direct(AttrWriter *aw, const ch
 		CacheStoreResult	acResult;
 		
 		acResult = ac_store_raw_data(ac, (char *)path, fa_dup(fa), TRUE, 
-                                curTimeMicros(), SKFS_DEF_ATTR_TIMEOUT_SECS * 1000);
+                                writeTimeMicros, SKFS_DEF_ATTR_TIMEOUT_SECS * 1000);
 		if (acResult != CACHE_STORE_SUCCESS) {
 			srfsLog(LOG_ERROR, "ac_store_raw_data_failed with %d at %s %d", acResult, __FILE__, __LINE__);
 			result = SKOperationState::FAILED;
