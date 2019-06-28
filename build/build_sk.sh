6064351d06fbca8fef7b3362153dbfd87a4d9ab9
Benjamin Holst <Benjamin.Holst@morganstanley.com>
2019-06-27 22:38:00 -0400

    fixed comma typo

--- build/build_sk.sh
+++ build/build_sk.sh
@@ -13,8 +13,8 @@ typeset output_filename=$(f_getBuildSk_RunOutputFilename)
     # f_checkForRequiredExecutables;    currently not working how I want it to. Need to check them from build.config
     f_cleanOrMakeBuildDirectory;
     f_cleanOrMakeInstallDirectory;
-    f_runBuildAntScript;
     # f_runBuildGradleScript;
+    f_runBuildAntScript;
     f_printSummary "$output_filename";
     f_printLocalElapsed;
  } 2>&1 | tee $output_filename
