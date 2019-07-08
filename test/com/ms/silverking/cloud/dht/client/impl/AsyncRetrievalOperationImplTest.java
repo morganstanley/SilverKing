38f78e572e4ecdb3bf385aede0fad2ea84df6d21
Hanna Peramotava <Hanna.Peramotava@morganstanley.com>
2019-07-08 13:16:24 +0100

    Add createFailureMessage function to KeyedOperationException instead of AsyncRetrievalOperationImpl

--- test/com/ms/silverking/cloud/dht/client/impl/AsyncRetrievalOperationImplTest.java
+++ test/com/ms/silverking/cloud/dht/client/impl/AsyncRetrievalOperationImplTest.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.ms.silverking.cloud.dht.client.FailureCause;
+import com.ms.silverking.cloud.dht.client.KeyedOperationException;
 import com.ms.silverking.cloud.dht.client.OperationException;
 import com.ms.silverking.cloud.dht.client.OperationState;
 import com.ms.silverking.cloud.dht.client.StoredValue;
@@ -20,10 +21,16 @@ import org.junit.Test;
 
 public class AsyncRetrievalOperationImplTest {
 
+    AsyncRetrievalOperationImpl<String, String> retrievalOperationMock;
+
     @Before
     public void init() {
         BaseWorker.class.getClassLoader().setClassAssertionStatus(BaseWorker.class.getName(), false);
         LWTPoolProvider.defaultNonConcurrentWorkPool = mock(LWTPool.class);
+
+        retrievalOperationMock = (AsyncRetrievalOperationImpl<String, String>) mock(AsyncRetrievalOperationImpl.class);
+        when(retrievalOperationMock.getOperationStateMap()).thenReturn(new HashMap<String, OperationState>());
+        when(retrievalOperationMock.getPartialResults()).thenReturn(new HashMap<String, StoredValue>());
     }
 
     @After
@@ -40,11 +47,7 @@ public class AsyncRetrievalOperationImplTest {
         failureCauses.put("Obj2", FailureCause.TIMEOUT);
 
         try {
-            AsyncRetrievalOperationImpl<String, String> retrievalOperationMock =
-                    (AsyncRetrievalOperationImpl<String, String>) mock(AsyncRetrievalOperationImpl.class);
             when(retrievalOperationMock.getFailureCauses()).thenReturn(failureCauses);
-            when(retrievalOperationMock.getOperationStateMap()).thenReturn(new HashMap<String, OperationState>());
-            when(retrievalOperationMock.getPartialResults()).thenReturn(new HashMap<String, StoredValue>());
             doCallRealMethod().when(retrievalOperationMock).throwFailedException();
 
             retrievalOperationMock.throwFailedException();
@@ -54,6 +57,28 @@ public class AsyncRetrievalOperationImplTest {
                 assert (errorMessage.contains(
                         String.format("Error for %s caused by %s", entry.getKey(), entry.getValue().name())));
             }
+        }
+    }
+
+    @Test
+    public void testThrowFailedExceptionLimitsKeys() {
+
+        Map<String, FailureCause> failureCauses = new HashMap<String, FailureCause>();
+        int keysLimit = KeyedOperationException.getKeysLimit();
+        int failureNum = 20;
+        while (failureNum > 0) {
+            failureCauses.put("Obj" + failureNum, FailureCause.ERROR);
+            failureNum--;
+        }
+
+        try {
+            when(retrievalOperationMock.getFailureCauses()).thenReturn(failureCauses);
+            doCallRealMethod().when(retrievalOperationMock).throwFailedException();
+
+            retrievalOperationMock.throwFailedException();
+        } catch (OperationException e) {
+            String errorMessage = e.getMessage();
+            assert (errorMessage.split(KeyedOperationException.getDelimiter()).length == keysLimit);
         }
     }
 }
