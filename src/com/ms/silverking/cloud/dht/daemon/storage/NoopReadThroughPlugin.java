a15a40b1a8eec14e917717b9e34fa080b15d32af
Stephen Webb <Stephen.Webb@morganstanley.com>
2019-07-18 17:28:43 +0100

    Add read through plugin with noop implementation

---
+++ src/com/ms/silverking/cloud/dht/daemon/storage/NoopReadThroughPlugin.java
@@ -0,0 +1,23 @@
+package com.ms.silverking.cloud.dht.daemon.storage;
+
+import java.nio.ByteBuffer;
+
+import com.ms.silverking.cloud.dht.common.DHTKey;
+
+final class NoopReadThroughPlugin extends ReadThroughPlugin {
+
+  @Override
+  protected ByteBuffer[] readThroughBatchImpl(DHTKey[] keys, byte[] userOptions) {
+    return new ByteBuffer[keys.length];
+  }
+
+  @Override
+  protected ByteBuffer readThroughSingleImpl(DHTKey key, byte[] userOptions) {
+    return null;
+  }
+
+  @Override
+  String getName() {
+    return this.getClass().getCanonicalName();
+  }
+}
