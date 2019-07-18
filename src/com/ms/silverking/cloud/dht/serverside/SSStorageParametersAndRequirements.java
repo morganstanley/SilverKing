c818db6857ee3c012ee7c4ec6961dafb6e087f11
Glenn Judd <glenn.judd@morganstanley.com>
2019-07-18 15:36:58 -0400

    Add lockSeconds. Fix file segment reference leak. Add maxUnfinalizedDeletedBytes.

--- src/com/ms/silverking/cloud/dht/serverside/SSStorageParametersAndRequirements.java
+++ src/com/ms/silverking/cloud/dht/serverside/SSStorageParametersAndRequirements.java
@@ -1,5 +1,6 @@
 package com.ms.silverking.cloud.dht.serverside;
 
 public interface SSStorageParametersAndRequirements extends SSStorageParameters {
-	public long getRequiredPreviousVersion();
+    public long getRequiredPreviousVersion();
+    public short getLockSeconds();
 }
