9ad1b245f65bf2a349b4e4043ac747b415dcce91
Cyrus Lyons <Cyrus.Lyons@morganstanley.com>
2019-09-03 13:59:14 +0100

    expose ring master in SSNamespaceStore

--- src/com/ms/silverking/cloud/dht/serverside/SSNamespaceStore.java
+++ src/com/ms/silverking/cloud/dht/serverside/SSNamespaceStore.java
@@ -8,6 +8,7 @@ import com.ms.silverking.cloud.dht.NamespaceOptions;
 import com.ms.silverking.cloud.dht.NamespaceVersionMode;
 import com.ms.silverking.cloud.dht.common.DHTKey;
 import com.ms.silverking.cloud.dht.common.OpResult;
+import com.ms.silverking.cloud.dht.daemon.NodeRingMaster2;
 
 public interface SSNamespaceStore {
     public long getNamespaceHash();
@@ -19,4 +20,5 @@ public interface SSNamespaceStore {
     public ByteBuffer retrieve(DHTKey key, SSRetrievalOptions options);
     public ByteBuffer[] retrieve(DHTKey[] keys, SSRetrievalOptions options);
     public ReadWriteLock getReadWriteLock();
+    public NodeRingMaster2 getRingMaster();
 }
