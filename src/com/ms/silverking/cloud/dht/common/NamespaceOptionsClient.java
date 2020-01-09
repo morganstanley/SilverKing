76910caa727f366995de7f473c00d183c93d60a8
Glenn Judd <glenn.judd@morganstanley.com>
2020-01-09 14:20:54 -0500

    Support for legacy Jace proxy generation

---
+++ src/com/ms/silverking/cloud/dht/common/NamespaceOptionsClient.java
@@ -0,0 +1,9 @@
+package com.ms.silverking.cloud.dht.common;
+
+import com.ms.silverking.cloud.dht.client.impl.NamespaceOptionsClientCS;
+
+/**
+ * For backwards compatibility. New code should use NamespaceOptionsClientCS.
+ */
+public interface NamespaceOptionsClient extends NamespaceOptionsClientCS {
+}
