1c52ff63302f608f00d6733b7eb435c429cf18da
Glenn Judd <glenn.judd@morganstanley.com>
2020-04-06 20:36:23 -0400

    Callback test

---
+++ test/com/ms/silverking/cloud/dht/client/TestCallbackDHT.java
@@ -0,0 +1,68 @@
+package com.ms.silverking.cloud.dht.client;
+
+import java.io.IOException;
+import java.util.concurrent.Semaphore;
+
+import com.ms.silverking.cloud.dht.gridconfig.SKGridConfiguration;
+
+public class TestCallbackDHT implements AsyncOperationListener {    
+    private Semaphore    s;
+    
+    public void runTest(SKGridConfiguration gridConfig, int iterations) {
+        try {
+            Namespace   ns;
+            AsynchronousNamespacePerspective<String, String>    asyncNSP;
+            AsyncPut<String>    asyncPut;
+            AsyncSingleValueRetrieval<String,String>    asyncGet;
+
+            s = new Semaphore(0);
+            ns = new DHTClient().openSession(gridConfig).createNamespace("MyNamespace" + System.currentTimeMillis());
+            asyncNSP = ns.openAsyncPerspective(String.class, String.class);
+            for (int i = 0; i < iterations; i++) {
+                String  key;
+                
+                System.out.printf("Iteration %d\n", i);
+                System.out.println("Calling put");
+                key = "key"+ i;
+                asyncPut = asyncNSP.put(key, "value");
+                System.out.println("Adding listener");
+                asyncPut.addListener(this, OperationState.SUCCEEDED, OperationState.FAILED, OperationState.INCOMPLETE);
+                //asyncPut.addListener(this);
+                System.out.println("Acquiring semaphore");
+                s.acquire();
+                s.drainPermits();
+                System.out.println("Calling get");
+                asyncGet = asyncNSP.get(key);
+                System.out.println("Adding listener");
+                asyncGet.addListener(this);
+                System.out.println("Acquiring semaphore");
+                s.acquire();
+                System.out.printf("%s\n", asyncGet.getValue());
+            }
+            System.out.println("Exiting test");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    @Override
+    public void asyncOperationUpdated(AsyncOperation asyncOperation) {
+        if (asyncOperation.getState() == OperationState.FAILED) {
+            System.out.printf("Operation failed: %s\n", asyncOperation);
+        } else if (asyncOperation.getState() == OperationState.INCOMPLETE) {
+            System.out.printf("Operation incomplete: %s\n", asyncOperation);
+        } else {
+            System.out.printf("Complete %s\n", asyncOperation);
+            s.release();
+        }
+        System.out.printf("%s\n", Thread.currentThread().getName());
+    }
+    
+    public static void main(String[] args) throws IOException {
+        try {
+            new TestCallbackDHT().runTest( SKGridConfiguration.parseFile(args[0]), Integer.parseInt(args[1]) );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
